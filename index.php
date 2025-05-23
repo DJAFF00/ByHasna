ab8955501d6bd867f3710e84a8ea16643e4f56f5
DJAFF00 <djaffmanakel259451@gmail.com>
2025-05-23 18:06:26 +0100

    commit favicon

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 <html lang="en">
 
 <head>
-	<title>BookSaw - Free Book Store HTML CSS Template</title>
+	<title>ByHasna-Livre personnalisé</title>
+	<link rel="icon" type="image/png" href="images/favicon.png">
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
